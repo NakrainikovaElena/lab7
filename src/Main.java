17b9650261b0504f1bf2a9a8d39fe4eac31d24de
Elena <enakrainikova@mail.ru>
2025-05-01 20:05:31 +0400

    Реализация посетителей
    Реализованы конкретные посетители Session и Vacations

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,16 @@ public class Main {
     public static void main(String[] args) {
         Persona student = new Student();
         Persona professor = new Professor();
+
+        Visitor session = new Session();
+        student.accept(session);
+        professor.accept(session);
+
+        System.out.println("---");
+
+        Visitor vacations = new Vacations();
+        student.accept(vacations);
+        professor.accept(vacations);
     }
 }
 
@@ -34,3 +44,26 @@ class Professor implements Persona {
     public void goOnVacation() { System.out.println("Профессор идет в отпуск."); }
 }
 
+class Session implements Visitor {
+    @Override
+    public void visitStudent(Student student) {
+        student.takeExam();
+    }
+
+    @Override
+    public void visitProfessor(Professor professor) {
+        professor.conductExam();
+    }
+}
+
+class Vacations implements Visitor {
+    @Override
+    public void visitStudent(Student student) {
+        student.relax();
+    }
+
+    @Override
+    public void visitProfessor(Professor professor) {
+        professor.goOnVacation();
+    }
+}
